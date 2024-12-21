3b7b4c96388c0319eb7c2519259096312e4b830b
tushrahuja <ahuja.tushar.m@gmail.com>
2024-12-21 17:50:57 +0530

    Rotated Array Problem

--- Array/LeetcodeNo33.java
+++ Array/LeetcodeNo33.java
@@ -3,8 +3,8 @@ public class LeetcodeNo33 {
 
     public static void main(String[] args) {
         int[] nums = {4, 5, 6, 7, 0, 1, 2};
-        // int target = 0;
-        System.out.println(findPivot(nums));
+        int target = 0;
+        System.out.println(search(nums, target));
     }
 
     static int search(int[] nums, int target){
