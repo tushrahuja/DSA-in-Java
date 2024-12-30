5dc3f4a7d50d9cd54db3e7aec828eccef302f02f
tushrahuja <ahuja.tushar.m@gmail.com>
2024-12-31 00:08:02 +0530

    Insertion Sort

--- Array/Sorting/InsertionSort.java
+++ Array/Sorting/InsertionSort.java
@@ -3,27 +3,22 @@ import java.util.Arrays;
 
 public class InsertionSort {
   public static void main(String[] args) {
-    int[] arr = { 5, -3, 4, 2, -2, 1, 2, 2, 3, -1};
+    int[] arr = { 5, -3, 4, 2, -2, 1, 2, 2, 3, -1 };
     insertionSort(arr);
     System.out.println(Arrays.toString(arr));
   }
 
   static void insertionSort(int[] arr) {
-
-    for (int idx = 0; idx < arr.length - 1; idx++) {
-      for (int i = 0; i <= arr.length - 2; i++) {
-        for (int j = i + 1; j <= arr.length - 1; j++) {
-            if (arr[i] > arr[j]) {
-              int temp = arr[i];
-              arr[i] = arr[j];
-              arr[j] = temp;
-            }else{
-              break;
-            }
-          }
+    for (int i = 0; i < arr.length - 1; i++) {
+      for (int j = i + 1; j > 0; j--) {
+        if (arr[j] < arr[j - 1]) {
+          int temp = arr[j - 1];
+          arr[j - 1] = arr[j];
+          arr[j] = temp;
+        } else {
+          break;
+        }
       }
-
     }
-
   }
 }
