76f5e19042856eeb4a535a96ce296e2634208c22
tushrahuja <ahuja.tushar.m@gmail.com>
2025-01-06 22:47:58 +0530

    Fibonacci Series

--- Recursion/FibonacciProgram.java
+++ Recursion/FibonacciProgram.java
@@ -1,19 +1,13 @@
 public class FibonacciProgram {
   public static void main(String[] args) {
-    int n = 10;
-    for (int i = 0; i < n; i++) {
-      
-    }
-    
     System.out.println(fibo(5));
   }
 
-  static int fibo(int n){
-
-    if(n < 2){
+  static int fibo(int n) {
+    if (n < 2) {
       return n;
     }
 
-    return fibo(n-1) + fibo(n-2);
+    return fibo(n - 1) + fibo(n - 2);
   }
 }
