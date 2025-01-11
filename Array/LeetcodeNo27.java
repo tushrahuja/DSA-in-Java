3ad58d3c9413e5be81799be16c81bd2d6e88506c
tushrahuja <ahuja.tushar.m@gmail.com>
2025-01-12 02:56:11 +0530

    Remove Element

---
+++ Array/LeetcodeNo27.java
@@ -0,0 +1,18 @@
+
+public class LeetcodeNo27 {
+  public static void main(String[] args) {
+    int[] nums = {0,1,2,2,3,0,4,2};
+    int val = 2;
+    System.out.println(removeElement(nums, val));
+  }
+  static int removeElement(int[] nums, int val) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i]!=val){
+              nums[count] = nums[i];
+              count++;
+            }
+        }
+        return count;
+    }
+}
