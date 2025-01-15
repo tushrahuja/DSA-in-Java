def52ec409efe778b40d851dab6a27b0bbcf4933
tushrahuja <ahuja.tushar.m@gmail.com>
2025-01-16 00:12:04 +0530

    single number

--- Mathematics/LeetcodeNo136.java
+++ Mathematics/LeetcodeNo136.java
@@ -1,22 +1,18 @@
 
-import java.util.Arrays;
 
 public class LeetcodeNo136 {
   public static void main(String[] args) {
-    int[] nums = {2,2,1};
+    int[] nums = { 3, 3, 1, 6, 1, 7, 7};
     System.out.println(singleNumber(nums));
   }
 
   static int singleNumber(int[] nums) {
-    Arrays.sort(nums);
-    if(nums.length == 1){
-        return nums[0];
+    int unique = 0;
+
+    for (int i : nums) {
+      unique ^= i;
     }
-      for (int i = 0; i < nums.length - 1; i+=2) {
-        if(nums[i] != nums[i+1]){
-          return nums[i];
-        }
-      }
-      return nums[nums.length -1];
+
+    return unique;
   }
 }
